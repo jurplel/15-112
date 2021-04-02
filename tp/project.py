3f4ceaddd9c952647030665fdc6dacb3ec0bf29e
jurplel <jeep70cp@gmail.com>
2021-04-01 20:50:15 -0400

    organization progress

--- tp/project.py
+++ tp/project.py
@@ -7,87 +7,55 @@ import math, time
 
 import importer
 
+from threedee import Vector3D 
+
 
 def appStarted(app):
-    app.model = importer.importPly("spherey.ply")
+    app.model = importer.importPly("cube.ply")
+    targetFps = 144
+    app.timerDelay = 1000//targetFps
     app.started = time.time()
 
 def drawPolygon(app, canvas, polygon):
     v0 = polygon[0]
-    x0 = v0[0]
-    y0 = v0[1]
-    z0 = v0[2]
     v1 = polygon[1]
-    x1 = v1[0]
-    y1 = v1[1]
-    z1 = v1[2]
     v2 = polygon[2]
-    x2 = v2[0]
-    y2 = v2[1]
-    z2 = v2[2]
-    canvas.create_line(x0, y0, x1, y1)
-    canvas.create_line(x1, y1, x2, y2)
-    canvas.create_line(x2, y2, x0, y0)
-
-def getProjMatrix(app):
-    zFar = 100
-    zNear = 1
-    fov = math.radians(90.0)
-    aRatio = app.height/app.width
-    projectionMatrix = np.array([
-        [aRatio*(1/math.tan(fov/2)), 0, 0, 0],
-        [0, 1/math.tan(fov/2), 0, 0],
-        [0, 0, zFar/(zFar-zNear), 1],
-        [0, 0, -((zFar*zNear)/(zFar-zNear)), 0]
-    ])
-    return projectionMatrix
-
-def getScreenMatrix(app):
-    screenMatrix = [
-        [app.width/2, 0, 0, app.width/2],
-        [0, -app.height/2, 0, app.height/2],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ]
-    return screenMatrix
+    canvas.create_line(v0.x, v0.y, v1.x, v1.y)
+    canvas.create_line(v1.x, v1.y, v2.x, v2.y)
+    canvas.create_line(v2.x, v2.y, v0.x, v0.y)
 
 def redrawAll(app, canvas):
-    projectionMatrix = getProjMatrix(app)
-    screenMatrix = getScreenMatrix(app)
     for poly in app.model.polys:
-        projectedPoly = [[], [], [], []]
+        projectedPoly = []
         for i, vertex in enumerate(poly):
-            projectedVertex = np.append(vertex, 1)
-
-            theta = 1.0 * app.started-time.time()
+            vector = Vector3D.fromArray(vertex)
+            theta = 8.0 * (time.time()-app.started)
             theta2 = theta/2
-
-            rotZMatrix = [
-                [math.cos(theta), math.sin(theta), 0, 0],
-                [-math.sin(theta), math.cos(theta), 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]
-            ]
-
-            rotXMatrix = [
-                [1, 0, 0, 0],
-                [0, math.cos(theta2), math.sin(theta2), 0],
-                [0, -math.sin(theta2), math.cos(theta2), 0],
-                [0, 0, 0, 1]
-            ]
-
-            projectedVertex = projectedVertex @ rotXMatrix
-            projectedVertex = projectedVertex @ rotZMatrix
-
-            projectedVertex[0] += 1.5
-            projectedVertex[1] -= 0.5
-            projectedVertex[2] += 3
-
-            projectedVertex = projectedVertex @ projectionMatrix
-            projectedVertex = projectedVertex @ screenMatrix
             
+            print(theta)
+            vector.rotX(theta)
+            vector.rotZ(theta2)
 
-            projectedPoly[i] = projectedVertex
+            vector.z += 4
+            vector.x += 0
+            vector.y += 0
+
+
+
+            fov = 90
+
+            vector.project(app.height, app.width, fov)
+
+            vector.x += 1
+            vector.x *= 0.5
+            vector.x *= app.width
+
+            vector.y += 1
+            vector.y *= 0.5
+            vector.y *= app.height
+
+
+            projectedPoly.append(vector)
         
         if not projectedPoly:
             return
--- tp/threedee.py
+++ tp/threedee.py
@@ -1,6 +1,92 @@
 from dataclasses import dataclass
 
 import numpy as np
+import math
+
+class Vector3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @classmethod
+    def fromArray(cls, array):
+        return cls(array[0], array[1], array[2])
+
+    def project(self, height, width, fov):
+        matrix = np.array([self.x, self.y, self.z, 1])
+
+        zFar = 100
+        zNear = 0.1
+        zDiff = zFar-zNear
+        fov = math.radians(90.0)
+        aRatio = height/width
+        fovCalculation = 1/math.tan(fov/2)
+
+        projectionMatrix = np.array([
+            [aRatio*fovCalculation, 0, 0, 0],
+            [0, fovCalculation, 0, 0],
+            [0, 0, zFar/zDiff, 1],
+            [0, 0, -(zFar*zNear)/zDiff, 0]
+        ])
+
+        matrix = matrix @ projectionMatrix
+        w = matrix[3]
+        self.x = matrix[0] / w
+        self.y = matrix[1] / w
+        self.z = matrix[2] / w
+
+    def rotX(self, deg):
+        matrix = np.array([self.x, self.y, self.z])
+
+        theta = math.radians(deg)
+
+        rotationMatrix = np.array([
+            [1, 0, 0],
+            [0, math.cos(theta), -math.sin(theta)],
+            [0, math.sin(theta), math.cos(theta)]
+        ])
+
+        matrix = matrix @ rotationMatrix
+        self.x = matrix[0]
+        self.y = matrix[1]
+        self.z = matrix[2]
+
+    def rotY(self, deg):
+        matrix = np.array([self.x, self.y, self.z])
+
+        theta = math.radians(deg)
+
+        rotationMatrix = np.array([
+            [math.cos(theta), 0, math.sin(theta)],
+            [0, 1, 0],
+            [-math.sin(theta), 0, math.cos(theta)]
+        ])
+
+        matrix = matrix @ rotationMatrix
+        self.x = matrix[0]
+        self.y = matrix[1]
+        self.z = matrix[2]
+
+    def rotZ(self, deg):
+        matrix = np.array([self.x, self.y, self.z])
+
+        theta = math.radians(deg)
+
+        rotationMatrix = np.array([
+            [math.cos(theta), -math.sin(theta), 0],
+            [math.sin(theta), math.cos(theta), 0],
+            [0, 0, 1]
+        ])
+
+        matrix = matrix @ rotationMatrix
+        self.x = matrix[0]
+        self.y = matrix[1]
+        self.z = matrix[2]
+
+    def toList(self):
+        return [self.x, self.y, self.z]
+        
 
 @dataclass
 class Mesh:
